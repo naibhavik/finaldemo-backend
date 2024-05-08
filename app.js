88e34cb077932ccb68acde835c8d084147304cba
bhavik <naibhavik68@gmail.com>
2024-05-08 11:36:05 +0530

    update in shocket-io

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 import express from "express";
+import http from 'http';
 import { dbConnection } from "./database/dbConnection.js";
-import jobRouter from "./routes/jobRoutes.js";
-import userRouter from "./routes/userRoutes.js";
-import applicationRouter from "./routes/applicationRoutes.js";
-import Message from "./models/messageSchema.js"
 import { config } from "dotenv";
 import cors from "cors";
-import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
-import http from 'http';
-import { Server } from "socket.io";
-const app = express();
-const server = http.createServer(app);
-import membershipRoute from "./routes/membershipRoutes.js"
+import socketRouter from "./routes/socketRouter.js"
+import userRouter from "./routes/userRoutes.js";
+import jobRouter from "./routes/jobRoutes.js";
+import applicationRouter from "./routes/applicationRoutes.js";
+import membershipRoute from "./routes/membershipRoutes.js";
+import { errorMiddleware } from "./middlewares/error.js";
 
 config({ path: "./config/config.env" });
+
+const app = express();
+const server = http.createServer(app);
 
 app.use(
   cors({
@@ -24,55 +24,7 @@ app.use(
     credentials: true,
   })
 );
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
 
-// socketRouter(io);
-const rooms = {};
-
-io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
-
-  socket.on("join-room", (roomId) => {
-    socket.join(roomId);
-
-    rooms[socket.id] = roomId;
-    console.log(`User ${socket.id} joined room ${roomId}`);
-  });
-
-  socket.on("send-message", async (message) => {
-    const roomId = rooms[socket.id];
-    if (roomId) {
-      io.to(roomId).emit("received-message", message);
-
-      // Save message to MongoDB
-      try {
-        await Message.create(message);
-        console.log("Message saved to database:", message);
-      } catch (error) {
-        console.error("Error saving message to database:", error);
-      }
-      try {
-        const previousMessages = await Message.find({ roomId });
-        socket.emit("previous-messages", previousMessages);
-      } catch (error) {
-        console.error("Error fetching previous messages:", error);
-      }
-    }
-  });
-
-  // socket.on("disconnect", () => {
-  //   const roomId = rooms[socket.id];
-  //   if (roomId) {
-  //     console.log(`User ${socket.id} disconnected from room ${roomId}`);
-  //     delete rooms[socket.id];
-  //   }
-  // });
-});
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -83,14 +35,15 @@ app.use(
   })
 );
 
-
+socketRouter(server); // Initialize Socket.IO
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
-app.use("/api/create-checkout-session", membershipRoute)
-// app.use("/api/user", userRouter);
+app.use("/api/create-checkout-session", membershipRoute);
+
 dbConnection();
 
 app.use(errorMiddleware);
+
 export default server;
--- routes/socketRouter.js
+++ routes/socketRouter.js
@@ -1,60 +1,55 @@
-// // socketRouter.js
-// import Message from "../models/messageSchema.js";
-// const rooms = new Map();
+import { Server } from "socket.io";
+import Message from "../models/messageSchema.js"
 
+const socketRouter = (server) => {
+	const io = new Server(server, {
+		cors: {
+			origin: "*",
+			methods: ["GET", "POST"],
+		},
+	});
 
-// //rooms.set("mongo_id", "socket_id")
+	const rooms = {};
 
-// const socketRouter = (io) => {
-// 	io.on("connection", (socket) => {
-// 		console.log(`User connected: ${socket.id}`);
+	io.on("connection", (socket) => {
+		console.log(`User connected: ${socket.id}`);
 
-// 		// socket.on('testing-123', (data) => {
-// 		// 	console.log('Received testing-123 event with data:', data);
-// 		// 	// Handle the event data here
-// 		// });
+		socket.on("join-room", (roomId) => {
+			socket.join(roomId);
 
-// 		socket.on("join-room", (roomId) => {
-// 			socket.join(roomId);
-// 			rooms[socket.id] = roomId;
-// 			const receiver_socket_id = rooms.get("mongo_id")
-// 			console.log(`User ${socket.id} joined room ${roomId}`);
-// 		});
+			rooms[socket.id] = roomId;
+			console.log(`User ${socket.id} joined room ${roomId}`);
+		});
 
-// 		socket.on("send-message", async (message) => {
-// 			const roomId = rooms[socket.id];
-// 			if (roomId) {
-// 				io.to(roomId).emit("received-message", message);
+		socket.on("send-message", async (message) => {
+			const roomId = rooms[socket.id];
+			if (roomId) {
+				io.to(roomId).emit("received-message", message);
 
-// 				// Save message to MongoDB
-// 				try {
-// 					await Message.create(message);
-// 					console.log("Message saved to database:", message);
-// 				} catch (error) {
-// 					console.error("Error saving message to database:", error);
-// 				}
+				// Save message to MongoDB
+				try {
+					await Message.create(message);
+					console.log("Message saved to database:", message);
+				} catch (error) {
+					console.error("Error saving message to database:", error);
+				}
+				try {
+					const previousMessages = await Message.find({ roomId });
+					socket.emit("previous-messages", previousMessages);
+				} catch (error) {
+					console.error("Error fetching previous messages:", error);
+				}
+			}
+		});
 
-// 				try {
-// 					const previousMessages = await Message.find({ roomId });
-// 					socket.emit("previous-messages", previousMessages);
-// 				} catch (error) {
-// 					console.error("Error fetching previous messages:", error);
-// 				}
-// 			}
-// 		});
+		socket.on("disconnect", () => {
+			const roomId = rooms[socket.id];
+			if (roomId) {
+				console.log(`User ${socket.id} disconnected from room ${roomId}`);
+				delete rooms[socket.id];
+			}
+		});
+	});
+};
 
-// 		socket.on("disconnect", () => {
-// 			const roomId = rooms[socket.id];
-// 			if (roomId) {
-// 				console.log(`User ${socket.id} disconnected from room ${roomId}`);
-// 				delete rooms[socket.id];
-// 			}
-// 		});
-// 	});
-
-
-
-
-// };
-
-// export default socketRouter;
+export default socketRouter;
